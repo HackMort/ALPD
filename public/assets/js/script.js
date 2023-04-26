9d29c215b6c16c7f9fe2c2e8720ac1be80ca6f4b
Jonathan Juarez <jonathan.juarezwp@gmail.com>
2023-04-26 10:47:44 -0600

    Add Mobile Menu Inital Functionality

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -22,4 +22,41 @@ document.addEventListener('DOMContentLoaded', function (e) {
    * Call the function from accordions.js
   */
   Accordions()
+
+  // Toggle Menu Button
+  const mobileMenuToogleBtn = document.getElementById('toggle-menu-button')
+  const mobileNav = document.querySelector('.mobile__menu')
+  if (mobileMenuToogleBtn) {
+    mobileMenuToogleBtn.addEventListener('click', () => {
+      mobileMenuToogleBtn.classList.toggle('is--open')
+      mobileNav.classList.toggle('is--open')
+    })
+  }
+
+  // Menu Dropdown
+  const menuItems = document.querySelectorAll('.primary__navigation li a')
+  const mobileQuery = window.matchMedia('(max-width: 1024px)')
+  const currentPage = window.location.pathname.replace(/\/+$/, '') // Remove trailing slash if any, this is needed in order to work in the Dev Server.
+  menuItems.forEach((item) => {
+    if (item.getAttribute('href') === currentPage) {
+      item.classList.add('current-page')
+      if (item.classList.contains('nav__sub_menu_item_link')) {
+        const parent = item.closest('.has__sub_menu')
+        parent.classList.add('current-page')
+      }
+    }
+    item.addEventListener('click', (e) => {
+      if (!mobileQuery.matches) {
+        return
+      }
+      if (item.parentElement.classList.contains('has__sub_menu')) {
+        e.preventDefault()
+        item.parentElement.classList.toggle('is--open')
+        // const subNav = item.nextElementSibling
+        // if (subNav) {
+        //   subNav.classList.toggle('is--open')
+        // }
+      }
+    })
+  })
 })
