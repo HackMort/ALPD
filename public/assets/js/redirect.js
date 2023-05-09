eaa2bd11816a52a9eb8975be053dbc8931644029
Jonathan Juarez <jonathan.juarezwp@gmail.com>
2023-05-09 15:09:34 -0600

    Refactor Redirect Script for Landing Page

--- public/assets/js/redirect.js
+++ public/assets/js/redirect.js
@@ -1,7 +1,7 @@
-const enterSiteURL = 'enter-site.html'
+const enterSiteURL = '/enter-site/'
 const currentHref = window.location.href
 const recurrentVisitor = window.localStorage.getItem('recurrent-visitor') === 'true'
 
-if (!recurrentVisitor && !currentHref.includes(enterSiteURL)) {
+if (!recurrentVisitor && !currentHref.includes(enterSiteURL) && window.location.pathname !== '/validate/') {
   window.location.href = enterSiteURL
 }
